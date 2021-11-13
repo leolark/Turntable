5348fa132f4d9d89bfba9cd6710576ab29b50ed4
Leonte Zhavoronkov <32207706+leolark@users.noreply.github.com>
2021-11-13 21:06:11 +0300

    Update script.js

--- Turntable/script.js
+++ Turntable/script.js
@@ -6,7 +6,7 @@ function onlyTime() {
     if (h>22 || h<5) document.getElementById("style").href = "night.css";
     if (h>4 && h<12) document.getElementById("style").href = "morning.css";
     if (h>11 && h<18) document.getElementById("style").href = "day.css";
-    if (h>20 && h<23) document.getElementById("style").href = "evening.css";
+    if (h>17 && h<23) document.getElementById("style").href = "evening.css";
 }
 
 let onlyTimer = setInterval(() => onlyTime(), 100);
